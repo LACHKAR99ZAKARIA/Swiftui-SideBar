d6204c19eb59bd2d43e21cc67a13e90828354485
LACHKAR Zakaria <lachkar99zakaria@gmail.com>
2024-02-14 09:56:20 +0100

    Initial Commit

---
+++ Swiftui SideBar/Swiftui_SideBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_SideBarApp.swift
+//  Swiftui SideBar
+//
+//  Created by Zakaria Lachkar on 14/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_SideBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
